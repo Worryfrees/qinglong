bbc4e2b249ba8efa6cc2b8f7fba06043516e7bb1
Worryfree <xxbb@live.com>
2022-09-08 11:25:45 +0800

    update

---
+++ script/jd.js
@@ -0,0 +1,9 @@
+ql repo https://github.com/smiek2121/scripts.git "jd_|gua_" "" "ZooFaker_Necklace.js|JDJRValidator_Pure.js|sign_graphics_validate.js|cleancart_activity.js|jdCookie.js|sendNotify.js"
+
+ql repo https://github.com/he1pu/signin.git "_|si_" "activity|backUp" "tools"
+
+ql repo https://github.com/leafTheFish/DeathNote.git	
+
+ql repo https://github.com/feverrun/my_scripts.git "jd_|jx_|jddj_|getCookie|getJDCookie" "backUp/activity|backUp/card|backUp/py|backUp/utils/|backUp/test|jd_fruits.js|jd_pet.js|jd_factory.js|jd_health.js|jd_sgmh.js|jd_dreamFactory.js|jd_plantBean.js" "^(jd|JD|JS)[^_]|USER|sendNotify|utils"	
+
+ql repo https://github.com/KingRan/KR.git "jd_|jx_|jdCookie" "activity|backUp" "^jd[^_]|USER|utils|function|sign|sendNotify|ql|JDJR"	
